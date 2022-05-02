776899f8b5a86af9872ab5deb5f7cd6b86fb2aeb
CyberZen <75082239+Muhamohsen@users.noreply.github.com>
2022-05-02 13:07:44 +0300

    Update ServerSetup.sh
    chnage the gophish installtion location so it would not be affected by the user

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -562,7 +562,7 @@ function Install_GoPhish {
 	#go get github.com/gophish/gophish
 	#cd /root/go/src/github.com/gophish/gophish
 	#go build 
-	cd ~
+	cd /etc/
 	wget https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip -O gophish.zip
 	mkdir gophish
 	unzip gophish.zip -d ./gophish
